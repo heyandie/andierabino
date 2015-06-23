afcedc7716146a88deaddfda44896f67574fd344
Andie Rabino <rabinoandie@gmail.com>
2015-06-23 15:18:04 +0800

    fixed stylesheets link

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Heyandie
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     # Make public assets requireable in manifest files
-    config.assets.paths << Rails.root.join("public", "assets", "stylesheets")
-    config.assets.paths << Rails.root.join("public", "assets", "javascripts")
+    # config.assets.paths << Rails.root.join("public", "assets", "stylesheets")
+    # config.assets.paths << Rails.root.join("public", "assets", "javascripts")
   end
 end
