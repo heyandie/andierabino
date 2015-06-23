9b37ee1f41950a868c6a214b3d4b524943c3cd6e
Andie Rabino <rabinoandie@gmail.com>
2015-06-23 17:11:26 +0800

    enabled caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_controller.perform_caching = true
   config.static_cache_control = "public, max-age=31536000"
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -23,7 +21,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_assets = true;
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
