97edef4274b35143a4b063a560ee42f03b478990
Andie Rabino <rabinoandie@gmail.com>
2016-03-30 19:45:01 +0800

    added share counts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
+import urllib.request
+import json
+
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.urlresolvers import reverse
 
@@ -10,6 +13,17 @@ def index(request):
 
 def view_post(request, slug):
     post = get_object_or_404(Blog, slug=slug)
+
+    rest_url_a = "http://api.facebook.com/restserver.php?format=json&method=links.getStats&urls="+"http://"+post.url
+    rest_url_b = rest_url_a.replace('http://www.heyandie.com','heyandie.herokuapp.com')
+
+    response_a = urllib.request.urlopen(rest_url_a)
+    response_b = urllib.request.urlopen(rest_url_b)
+
+    data_a = json.loads(response_a.read().decode('utf-8'))
+    data_b = json.loads(response_b.read().decode('utf-8'))
+
+
     og_tags = {
         'fb:app_id': 224598357874885,
         'og:type': 'article',
@@ -26,5 +40,6 @@ def view_post(request, slug):
         'post': post,
         'title': title,
         'posts': Blog.objects.exclude(id__in=(post.id,)).order_by('-posted')[:3],
-        'header_class': "header-white"
+        'header_class': "header-white",
+        'share_count': data_a[0]['total_count'] + data_b[0]['total_count']
     })
