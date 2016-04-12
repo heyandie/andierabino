e3c282555e78979510e6544dca39632d5d92ee83
Andie Rabino <rabinoandie@gmail.com>
2016-04-12 16:54:14 +0800

    updated with work details

--- andierabino/urls.py
+++ andierabino/urls.py
@@ -6,7 +6,11 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    url('^$', views.index, name="index"),
     url(r'^admin/', include(admin.site.urls)),
-    url('', include('blog.urls'))
+    url(r'^about$', views.about, name="about"),
+    url(r'^work$', views.work, name="work"),
+    url(r'^work/roadmob$', views.work_roadmob, name="work"),
+    url(r'^work/resume-manager$', views.work_resume_manager, name="resume-manager"),
+    url('', include('blog.urls')),
+    url(r'^$', views.index, name="index"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- andierabino/views.py
+++ andierabino/views.py
@@ -8,3 +8,23 @@ def index(request):
     return render_to_response('index.html', {
         'posts': Blog.objects.all().order_by('-posted')[:6]
     })
+
+
+def about(request):
+
+    return render_to_response('about.html', {})
+
+
+def work(request):
+
+    return render_to_response('work.html')
+
+
+def work_roadmob(request):
+
+    return render_to_response('work/roadmob.html')
+
+
+def work_resume_manager(request):
+
+    return render_to_response('work/resume_manager.html')
